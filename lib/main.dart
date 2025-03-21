fc425b2c610a6062f4472b5a0c3fb6055c2b288d
rehman <rehmankh2256@gmail.com>
2025-03-21 15:07:30 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,42 @@
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import 'package:whatsapp/splash_screen.dart';
+// void main()
+// {
+//   runApp(MyApp());
+// }
+// class MyApp extends StatefulWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+//
+// class _MyAppState extends State<MyApp> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return GetMaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: splash_screen(),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:whatsapp/splash_screen.dart';
+import 'package:luckynumber/lucky_number.dart';
 void main()
 {
   runApp(MyApp());
 }
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: splash_screen(),
+      home: lucky(),
     );
   }
 }
